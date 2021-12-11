fa5ff756302c9832c731dab5f2d7b80e61b70dc7
rakiblegends <rakiblegends.std63@gmail.com>
2021-12-11 15:51:29 +0600

    STL

--- cp book 1/sieve.cpp
+++ cp book 1/sieve.cpp
@@ -1,17 +1,27 @@
 #include <bits/stdc++.h>
+#define M 100000
 using namespace std;
-vector <bool> primes(101,true);
+bitset <M> primes ;
 void sieve(){
-    primes[0]=false,primes[1]=false;
-    for(int i=2;i*i<=100;i++){
-        if(primes[i]==true){
-            for(int j=i*i;j<=100;j+=i){
-                primes[j]=false;
+    primes[0]=true,primes[1]=true;
+    for(int i=3;i*i<=M;i++){
+        if(primes[i]==false){
+            for(int j=i*i;j<=M;j+=i){
+                primes[j]=true;
             }
         }
     }
 }
 int main(){
-    
+    sieve();
+    vector<long long int> prime;
+    vector <long long int> :: iterator it;
+    prime.push_back(2);
+    for(int i=3;i<=M;i+=2){
+        if(primes[i]==false) prime.push_back(i);
+    }
+    for(it = prime.begin();it != prime.end();it++){
+        cout << *it << " ";
+    }
     return 0;
 }
