7203bb1ad3882fb9900d1f0accff5465405f6e54
Rakib Hossain <35700333+rakiblegends@users.noreply.github.com>
2023-05-30 03:34:15 +0600

    Update template.cpp
    Updated template for competitive programming.

--- template.cpp
+++ template.cpp
@@ -3,30 +3,25 @@ using namespace std;
  
 typedef long long ll;
 typedef long double ld;
-typedef pair<int,int> p32;
 typedef pair<ll,ll> p64;
-typedef pair<double,double> pdd;
 typedef vector<ll> v64;
-typedef vector<int> v32;
-typedef vector<vector<int> > vv32;
-typedef vector<vector<ll> > vv64;
-typedef vector<vector<p64> > vvp64;
 typedef vector<p64> vp64;
-typedef vector<p32> vp32;
-ll MOD = 998244353;
+ll mod = 1e9+7;
 double eps = 1e-12;
 #define forn(i,e) for(ll i = 0; i < e; i++)
-#define forsn(i,s,e) for(ll i = s; i < e; i++)
 #define rforn(i,s) for(ll i = s; i >= 0; i--)
-#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
-#define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
-#define INF 2e18
+#define nn "\n"
+#define inf 2e18
+#define setbit(x,k) (x|= (1LL<<k))
+#define checkbit(x,k) ((x>>k)&1)
+#define clearbit(x,k) (x&= ~(1LL<<k))
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
 #define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define all(x) (x).begin(), (x).end()
-#define sz(x) ((ll)(x).size())
  
 
 void solve(){
